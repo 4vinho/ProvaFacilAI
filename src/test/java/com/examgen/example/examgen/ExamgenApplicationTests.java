a622f95bbbd11f0f16a0147e02e994926d6f9b57
Eduardo Alves <155503937+4vinho@users.noreply.github.com>
2025-08-23 16:48:59 -0300

    feat: add dependencias

---
+++ src/test/java/com/examgen/example/examgen/ExamgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examgen.example.examgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
